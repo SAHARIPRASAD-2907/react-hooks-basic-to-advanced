76fc84b88105fa7df10c6144245150ab9d53e6f9
S A HARIPRASAD <myharicode@gmail.com>
2022-05-13 12:02:45 +0530

    compleated use state

--- pages/useEffectHook/index.js
+++ pages/useEffectHook/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 function UseEffectHook() {
-  return <div>UseEffectHook</div>;
+  return <div>
+    <a href={"https://blog.webdevsimplified.com/2020-04/use-effect/"}><h1>UseEffectHook</h1></a>
+
+  </div>;
 }
 
 export default UseEffectHook;
--- pages/useStateHook/index.js
+++ pages/useStateHook/index.js
@@ -1,16 +1,54 @@
+import {useState} from "react";
+import ReactMarkdown from 'react-markdown'
+
 function UseStateHook() {
+    const [counter,setCounter] =  useState(0);
+    const [count,setCount] = useState({count:0})
+    // regular
+   const addButton = (e)=>{
+       e.preventDefault()
+       setCounter((prevValue)=> prevValue+1)
+   }
+   // regular
+    const subButton = (e)=>{
+        e.preventDefault()
+        setCounter((prevValue)=> prevValue-1)
+    }
+    // object
+    const addCount = (e)=>{
+        e.preventDefault()
+        setCount((prevValue)=>{
+            return {...prevValue,count: prevValue.count + 1}
+        })
+    }
+    //object
+    const subCount = (e)=>{
+        e.preventDefault()
+        setCount((prevValue)=>{
+            return {...prevValue,count: prevValue.count - 1}
+        })
+    }
   return (
     <div>
       <a href="https://blog.webdevsimplified.com/2020-04/use-state/">
         <h1>use state blog</h1>
       </a>
       <>
-        <button>-</button>
-        <span>0</span>
-        <button></button>
+          <h1>without object</h1>
+        <button onClick={addButton}>+</button>
+        <span>{counter}</span>
+        <button onClick={subButton}>-</button>
       </>
+        <>
+            <h1>using object</h1>
+            <button onClick={addCount}>+</button>
+            <span>{count.count}</span>
+            <button onClick={subCount}>-</button>
+        </>
+
     </div>
   );
 }
 
 export default UseStateHook;
+

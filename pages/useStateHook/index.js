b2e6816651f33b36b366a0341c058d8851e901c3
S A HARIPRASAD <myharicode@gmail.com>
2022-05-13 11:10:42 +0530

    first commit

---
+++ pages/useStateHook/index.js
@@ -0,0 +1,16 @@
+function UseStateHook() {
+  return (
+    <div>
+      <a href="https://blog.webdevsimplified.com/2020-04/use-state/">
+        <h1>use state blog</h1>
+      </a>
+      <>
+        <button>-</button>
+        <span>0</span>
+        <button></button>
+      </>
+    </div>
+  );
+}
+
+export default UseStateHook;
